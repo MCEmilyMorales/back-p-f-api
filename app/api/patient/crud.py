e00a62a9cf1dd65341924bda245e0f3128553663
Emily Morales <15ruth05@gmail.com>
2025-03-06 18:22:39 -0300

    se agrego el post de paciente

--- app/api/patient/crud.py
+++ app/api/patient/crud.py
@@ -1,2 +1,14 @@
 from prisma.models import Paciente
 from prisma import Prisma
+from datetime import datetime as dt  # Importa correctamente
+from app.api.patient.paciente_shema import PacienteCreate
+
+async def create_paciente(db:Prisma, paciente: PacienteCreate) -> Paciente:
+    """Permite inserta a un nuevo paciente en la base de datos.
+    Recibe: instancia de base de datos, nombre y la fecha de muestra.
+    retorna un mensaje de que el paciente fue cargado con exito"""
+
+    await db.paciente.create(
+        data=paciente.model_dump()
+    )
+    return {"Paciente creado con exito"}
--- app/api/patient/route.py
+++ app/api/patient/route.py
@@ -1,3 +1,16 @@
-from fastapi import FastAPI, HTTPException
-from app.api.database import db
-from app.api.patient import crud
+from fastapi import APIRouter, HTTPException
+from app.api.database import db  # ✅ Importar la conexión de main.py
+from app.api.patient.paciente_shema import PacienteCreate
+from app.api.patient.crud import create_paciente
+router = APIRouter()
+
+
+@router.post("/", summary="Crear un paciente", description="Crea un paciente con nombre.")
+async def crear_paciente(paciente: PacienteCreate):
+    """Permite inserta a un nuevo paciente en la base de datos.
+    Recibe: instancia de base de datos, nombre y la fecha de muestra.
+    retorna un mensaje de que el paciente fue cargado con exito"""
+    if not db.is_connected():
+        await db.connect()  # 🔹 Asegurar que está conectado    
+    nuevo_paciente = await create_paciente(db, paciente)
+    return nuevo_paciente
