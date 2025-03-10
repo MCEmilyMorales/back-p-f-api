9881878f95276fba5abc77d50711527fafa99429
Emily Morales <15ruth05@gmail.com>
2025-03-10 17:59:12 -0300

    el crud del informe y la imagen funcionan correctamente

--- app/api/informe/route.py
+++ app/api/informe/route.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
 from app.api.database import db
 from app.api.informe import crud
+from app.api.informe.models import InformeCreate
 
 def add_informe_routes(app: FastAPI):
     @app.post("/informe/", tags=["Informe"])
-    async def create_informe(fecha_de_muestra:str,pacienteId:str, imagenes):
+    async def create_informe(informeCreate: InformeCreate):
         """Permite insertar a un nuevo informe en la base de datos.
         Recibe: instancia de base de datos, fecha de muestra, pacienteId e imagenes.
         retorna un mensaje de que el informe fue cargado con exito"""
-        await crud.create_informe(db, fecha_de_muestra, pacienteId,imagenes)
+        informe = await crud.create_informe(db, informeCreate)
+        return informe
 
     @app.get("/informe/", tags=["Informe"])
     async def list_informes():
@@ -20,8 +22,8 @@ def add_informe_routes(app: FastAPI):
     @app.get("/informe/{informe_id}", tags=["Informe"])
     async def get_informe_id(informe_id:str):
         """ Conseguir 1 informe segun su id."""
-        informe = await crud.get_informe_id(db, informe_id)
-        return informe
+        return await crud.get_informe_id(db, informe_id)
+        
     
     @app.delete("/informe/{informe_id}", tags=["Informe"])
     async def delete_informe_id(informe_id:str):
