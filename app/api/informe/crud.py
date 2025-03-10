ca2e83e82f14a193a1cdf023882a5be6ab0d6c34
Emily Morales <15ruth05@gmail.com>
2025-03-10 14:55:28 -0300

    Resuelvo conflictos en archivos .pyc

--- app/api/informe/crud.py
+++ app/api/informe/crud.py
@@ -2,19 +2,47 @@ from prisma.models import Informe
 from prisma import Prisma
 from typing import List
 from datetime import datetime
+from app.api.image import crud
+from fastapi import HTTPException
 
 async def create_informe(db: Prisma, fecha_de_muestra:str, pacienteId:str,imagenes: List[str]) -> Informe:
-    """Permite insertar un nuevo informe de un paciente en la base de datos.
-    Recibe: instancia de base de datos, fecha de muestra, pacienteId, y un array de imagenes."""
+    """Inserta un nuevo informe de un paciente en la base de datos.    
+    Recibe: 
+    - db: instancia de Prisma
+    - fecha_de_muestra: Fecha de la muestra en formato string
+    - pacienteId: ID del paciente
+    - imagenes: Lista de IDs de imágenes a conectar
+    """
+     # Convertir la fecha a formato ISO 8601
+    fecha_iso = datetime.fromisoformat(fecha_de_muestra).isoformat()
+    try:
+            # 1. Crear el informe sin conectar las imágenes
+            informe_principal = await db.informe.create(
+                data={
+                    "fecha_de_muestra": fecha_iso,
+                    "pacienteId": pacienteId,
+                    "status": "pendiente"  # Estado inicial
+                }
+            )
 
- # Convertir la fecha a formato ISO 8601 con hora
-    fecha_iso = datetime.fromisoformat(fecha_de_muestra).isoformat()  # "2025-03-06T00:00:00"
+            # 2. Llamar al método de creación de imágenes
+            exito = await crud.create_imagen(informe_id=informe_principal.id)
+            if not exito:
+                raise HTTPException(status_code=400, detail="Error en el proceso secundario")
 
-    # Convertir imagenes a la estructura correcta para Prisma
-    imagenes_data = [{"id": img_id} for img_id in imagenes]
+            # 3. Si el proceso de imágenes fue exitoso, actualizar el informe
+            actualizar_registro = await db.informe.update(
+                where={"id": informe_principal.id},
+                data={
+                    "status": "completo",
+                    "imagenes": {"connect": [{"id": img_id} for img_id in imagenes]}
+                }
+            )
 
-    await db.informe.create(data={"fecha_de_muestra":fecha_iso, "pacienteId":pacienteId,"imagenes": {"connect": imagenes_data}})  # Conectar imágenes existentes
-    return {"Informe creado con exito"}
+            return {"message": "Informe creado con éxito", "informe_id": actualizar_registro.id}
+
+    except Exception as exception:
+            raise HTTPException(status_code=500, detail=f"Error en la creación: {str(exception)}")
 
 async def get_all_informes(db: Prisma):
     """ Conseguir todos los informes de la base de datos.
