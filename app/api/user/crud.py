791cf7f980b68bf735245a7b15e661dfe53a09c2
Emily Morales <15ruth05@gmail.com>
2025-03-20 14:12:35 -0300

    token implementado

--- app/api/user/crud.py
+++ app/api/user/crud.py
@@ -12,63 +12,70 @@ load_dotenv()
 
 
 async def create_user(db: Prisma, nombre: str, mail: str, password: str) -> Usuario:
-    """Permite insertar un nuevo usuario en la base de datos.
-    Recibe: instancia de base de datos, nombre y contraseña.
-    Retorna: objeto Usuario"""
+    """Crea un nuevo usuario en la base de datos.
+    Parametros: instancia de la base de datos, nombre, correo electrónico y contraseña en texto plano (se debe hashear antes de almacenarla).
+    Retorna: objeto del usuario creado."""
     return await db.usuario.create(data={"nombre": nombre, "mail": mail,"password": password})
 
 
 async def get_user(db: Prisma, user_id: str) -> Usuario | None:
     """ Buscar un usuario por su ID en la base de datos.
-    Recibe: instancia de base de datos, ID.
-    Retorna: objeto Usuario o None"""
+    Parametros: instancia de la base de datos, ID del usuario.
+    Retorna: Usuario si existe, None si no se encuentra."""
     return await db.usuario.find_unique(where={"id": user_id})
 
 
-async def get_all_users(db: Prisma):
-    """ Obtener todos los usuarios en la base de datos.
-    Recibe: instancia de base de datos.
-    Retorna: lista de objetos Usuario.
-    """
+async def get_all_users(db: Prisma)-> list[Usuario]:
+    """ Obtiene todos los usuarios en la base de datos.
+    Parametros: instancia de la base de datos.
+    Retorna: Lista de usuarios."""
     return await db.usuario.find_many()
 
 
 async def delete_user(db: Prisma, user_id: str) -> bool:
-    """Elimina un usuario
-    Recibe: instancia de base de datos, ID.
-    Retorna: True si se elimino, False si no existe.
-    """
+    """Elimina un usuario por su ID.
+    Parametros: instancia de la base de datos, ID del usuario a eliminar.
+    Retorna: True si se eliminó correctamente, False si el usuario no existe."""
     user = await db.usuario.delete(where={"id": user_id})
     return bool(user)
 
 
-async def login(db: Prisma,usuario_login):
-     # el usuario existe???
-    if not usuario_login.username:
-        raise HTTPException(status_code=400, detail='Debes proporcionar un mail o un nombre')
+async def login(db: Prisma,form_data)-> dict:
+    """Autentica a un usuario y genera un token JWT.
+    Parametros: instancia de la base de datos, datos de autenticación con nombre de usuario o correo y contraseña.
+    Retorna: Token de acceso y tipo de token;
+            Si las credenciales no son correctas lanza HTTPException: Si las credenciales son incorrectas."""
+    username = form_data.username
     
-    user =  await get_user_email(db, usuario_login.username) or await get_user_nombre(db, usuario_login.username)
+    if '@' in username:
+        user =  await get_user_email(db, form_data.username)
+    else:
+        user = await get_user_nombre(db, form_data.username)
     
-    if not user or not await verificar_password(usuario_login.password, user.password):
-        raise HTTPException(status_code=400, detail="Verifique sus datos.")
+    if not user or not await verificar_password(form_data.password, user.password):
+        raise HTTPException(status_code=400, detail='Verifique sus datos.')
 
     # Generar token de acceso
     token_de_acceso = crear_token.crear_access_token(
-        data={"sub": user.id},
-        expiracion=timedelta(minutes=int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 15)))
+        data={'sub': user.id},
+        expiracion=timedelta(minutes=int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')))
     )
     
-    return {"access_token": token_de_acceso, "token_type": "bearer"}
+    return {'access_token': token_de_acceso, 'token_type': 'bearer'}
 
 async def hashear_password(password:str)->str:
-    """Hashea la contraseña de forma segura"""
+    """Hashea la contraseña de forma segura usando bcrypt.
+    Parametros: contraseña en texto plano.
+    Retorna: contraseña hasheada."""
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     hashed_password = hashed_password.decode('utf-8')  
     return hashed_password
 
 
 async def verificar_password(password: str, passwordBD: str) -> bool:
-    """Compara la contraseña ingresada con la almacenada en la base de datos."""
+    """Compara una contraseña ingresada con la almacenada en la base de datos.
+    Parametros: Contraseña ingresada por el usuario. Contraseña almacenada en la base de datos (hasheada).
+    Retorna: True si la contraseña es la correcta, False si no lo es."""
     try:
         # Verifica que las contraseñas sean iguales usando bcrypt
         compare = bcrypt.checkpw(password.encode('utf-8'), passwordBD.encode('utf-8'))
@@ -79,19 +86,23 @@ async def verificar_password(password: str, passwordBD: str) -> bool:
     
 
 async def get_user_email(db: Prisma, mail: str)-> Usuario:
-    """Obtiene un usuario por su email"""
+    """Obtiene un usuario por su email.
+    Parametro: instancia de la base de datos, mail del usuario..
+    Retorna: Usuario si existe, None si no se encuentra."""
     return await db.usuario.find_unique(where={"mail": mail})
 
 
 async def get_user_nombre(db: Prisma, nombre: str)-> Usuario:
-    """Obtiene un usuario por su email"""
+    """Obtiene un usuario por su nombre.
+    Parametro: instancia de la base de datos, nombre del usuario.
+    Retorna: Usuario si existe, None si no se encuentra."""
     return await db.usuario.find_unique(where={"nombre": nombre})
 
 
 async def update_email(db: Prisma, user_id: str, mail:str) -> bool:
-    """Permite actualizar el mail del usuario en la base de datos.
-    Recibe: instancia de base de datos, id y nuevo mail.
-    Retorna: true si se actualizo y false si el usuario con ese id no fue encontrado"""
+    """Actualiza el mail de un usuario en la base de datos.
+    Parametros: instancia de la base de datos, id del usuario y nuevo mail.
+    Retorna: True si la actualización fue exitosa, False si el usuario no fue encontrado."""
     user=await db.usuario.find_unique(where={"id": user_id})
     if not user:
         return False  # Usuario no encontrado
--- app/api/user/token/decodificar_token.py
+++ app/api/user/token/decodificar_token.py
@@ -1,5 +1,5 @@
 from fastapi import Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from dotenv import load_dotenv
 import os
